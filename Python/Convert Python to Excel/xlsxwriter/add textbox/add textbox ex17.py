bd6d4dd025527612a4922e7692ffa76d1a8e6c4e
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-01-01 09:45:57 +0800

    Create add textbox ex17.py

---
+++ Python/Convert Python to Excel/xlsxwriter/add textbox/add textbox ex17.py
@@ -0,0 +1,12 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('add_textbox_ex17.xlsx')
+worksheet = workbook.add_worksheet()
+
+text = 'Formatted textbox'
+
+worksheet.insert_textbox('A1', 'This is some text',
+                         {'url': 'https://github.com/jmcnamara',
+                          'tip': 'GitHub'})
+
+workbook.close()
