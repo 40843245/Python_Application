de8195952bcbc535eba9d2e9b1d7168a219f3b30
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-01-01 11:04:46 +0800

    Create memory management ex1.py

---
+++ Python/Convert Python to Excel/xlsxwriter/memory management/memory management ex1.py
@@ -0,0 +1,14 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('memory_management_ex1.xlsx')
+worksheet = workbook.add_worksheet()
+
+row_max=5
+col_max=7
+some_data="Hello"
+# Ok. With 'constant_memory' you must write data in row by column order.
+for row in range(0, row_max):
+    for col in range(0, col_max):
+        worksheet.write(row, col, some_data)
+
+workbook.close()
