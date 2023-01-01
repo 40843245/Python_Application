492f5f4246656398a46971a959c3c08813753683
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-01-01 08:59:00 +0800

    Create add table ex11.py

---
+++ Python/Convert Python to Excel/xlsxwriter/add table/add table ex11.py
@@ -0,0 +1,13 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('add_table_ex11.xlsx')
+worksheet = workbook.add_worksheet()
+
+data = [
+    ['Apples', 10000, 5000, 8000, 6000],
+    ['Pears',   2000, 3000, 4000, 5000],
+    ['Bananas', 6000, 6000, 6500, 6000],
+    ['Oranges',  500,  300,  200,  700],
+]
+worksheet.add_table('B3:F7', {'total_row': True})
+workbook.close()
