ff5b6b61b13702d07cf0044ac52cf10fa7525925
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-01-01 08:53:30 +0800

    Create add table ex6.py

---
+++ Python/Convert Python to Excel/xlsxwriter/add table/add table ex6.py
@@ -0,0 +1,14 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('add_table_ex6.xlsx')
+worksheet = workbook.add_worksheet()
+
+data = [
+    ['Apples', 10000, 5000, 8000, 6000],
+    ['Pears',   2000, 3000, 4000, 5000],
+    ['Bananas', 6000, 6000, 6500, 6000],
+    ['Oranges',  500,  300,  200,  700],
+]
+# Turn on highlighting for the first column in the table.
+worksheet.add_table('B3:F7', {'first_column': True})
+workbook.close()
