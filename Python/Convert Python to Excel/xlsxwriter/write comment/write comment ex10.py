e0f12401d1eff967947c786bc479f00365ff46a9
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-01-01 10:51:53 +0800

    Create write comment ex10.py

---
+++ Python/Convert Python to Excel/xlsxwriter/write comment/write comment ex10.py
@@ -0,0 +1,11 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('write_comment_ex10.xlsx')
+worksheet = workbook.add_worksheet()
+
+text = 'Formatted textbox'
+
+worksheet.write('A1', 'Hello')
+worksheet.write_comment('C3', 'Hello', {'start_row': 0})
+
+workbook.close()
