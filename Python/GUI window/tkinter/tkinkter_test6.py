51ea678297fce6dd5fd6a27fafe065f111e5f050
MathGeeksJay <75050655+40843245@users.noreply.github.com>
2023-02-07 22:07:04 +0800

    Add files via upload

---
+++ Python/GUI window/tkinter/tkinkter_test6.py
@@ -0,0 +1,43 @@
+import tkinter
+from tkinter import *
+
+if __name__ == '__main__':
+
+    def test():
+        root = tkinter.Tk()
+        
+        #Runtime Error
+        #root.minsize=(20,20)
+        #root.maxsize=(20,20)
+        
+        root.minsize(2000,2000)
+        root.maxsize(2000,2000)
+        
+        def CloseWindow(root=root):
+            print("The quit Button is clicked.")
+            root.destroy()
+            
+        def doit(root=root):
+            d = tkinter.simpledialog.Dialog(parent=root,title=[
+                              "This is a test dialog.  "
+                              "Would this have been an actual dialog, "
+                              "the buttons below would have been glowing "
+                              "in soft pink light.\n"
+                              "Do you believe this?",
+                         ["Yes", "No", "Cancel"]]
+                         )
+            
+            print(dir(d))
+            print("----------------------")
+            print(tkinter.simpledialog.askinteger("Spam", "Egg count", initialvalue=12*12))
+            print(tkinter.simpledialog.askfloat("Spam", "Egg weight\n(in tons)", minvalue=1,
+                           maxvalue=100))
+            print(tkinter.simpledialog.askstring("Spam", "Egg label"))
+        t = tkinter.Button(root, text='Test', command=doit)
+        t.pack()
+        
+        q = tkinter.Button(root, text='Quit', command=CloseWindow)
+        q.pack()
+        root.mainloop()
+
+    test()
